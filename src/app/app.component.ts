18b1f8f0119b35b45586d258b38d1c0288feb6e4
efdiaz <efdiaz.dev@gmail.com>
2023-10-26 01:21:31 -0500

    Aplicación estable v1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Country } from './country.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'parcial-2';
+  title = 'PARCIAL - PARTE 2';
+
+  countries: Country[] = [
+    new Country('Perú', 'Perú'),
+    new Country('España', 'España'),
+    new Country('Francia', 'Francia'),
+    new Country('China', 'China'),
+    new Country('Australia', 'Australia'),
+    new Country('Estados Unidos', 'Estados Unidos'),
+    new Country('México', 'México'),
+    new Country('Canadá', 'Canadá'),
+    new Country('Alemania', 'Alemania'),
+    new Country('Italia', 'Italia'),
+  ];
+
+  getStyleCountry(country: string) {
+    console.log(country);
+    if (country == 'Perú') {
+      return {
+        color: '#ff0000',
+        'background-color': '#ffffff',
+      };
+    } else if (country == 'España') {
+      return {
+        color: '#ffff00',
+        'background-color': '#ff0000',
+      };
+    } else {
+      return {
+        color: 'cornflowerblue',
+        'background-color': 'darkslategrey',
+      };
+    }
+  }
 }
---
+++ src/app/country.model.ts
@@ -0,0 +1,9 @@
+export class Country {
+    name: string;
+    country: string;
+  
+    constructor(name: string, country: string) {
+      this.name = name;
+      this.country = country;
+    }
+  }
